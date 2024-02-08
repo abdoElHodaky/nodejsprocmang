42ead1f2cc30077cfdd12e562e4ee87f47abb055
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-02-08 16:31:46 +0200

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -42,7 +42,8 @@ module.exports = {
       // useCreateIndex: true,
       useNewUrlParser: true,
       useUnifiedTopology: true,
-       auto_reconnect: true ,
+       //auto_reconnect: true 
+      ,
     },
   },
   jwt: {
