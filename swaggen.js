8f73e640cf861a1014edf180d080ffb4e5cf9863
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-02-09 11:06:33 +0200

    Update swaggen.js

--- swaggen.js
+++ swaggen.js
@@ -1,7 +1,12 @@
 const swaggerAutogen = require('swagger-autogen')();
 
 const outputFile = './src/swagger.json';
-const endpointsFiles = ['./src/routes/v1/*.js'];
+const endpointsFiles = [
+                        './src/routes/v1/auth.route.js',
+                        './src/routes/v1/order.route.js',
+                        './src/routes/v1/user.route.js',
+                        './src/routes/v1/checklist.route.js'
+                       ];
 
 const config = {
     info: {
